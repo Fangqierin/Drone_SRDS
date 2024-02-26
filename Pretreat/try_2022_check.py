21784aaca46b134269a20e7d147f5b9b416f2443
Fangqierin <45373676+Fangqierin@users.noreply.github.com>
2024-02-25 22:42:11 -0600

    Delete Pretreat directory

--- Pretreat/try_2022_check.py
+++
@@ -1,5 +0,0 @@
-nohup python3 C_WSP_update_2022_check.py 5 dy2022_16 2 2 0 1 >& /tmp/log_sum_drone_4_16_3_120_0.txt 2>&1  &
-nohup python3 C_WSP_update_2022_check.py 5 dy2022_16 2 2 0 2 >& /tmp/log_sum_drone_4_16_3_120_0.txt 2>&1  &
-nohup python3 C_WSP_update_2022_check.py 5 dy2022_16 2 2 0 3 >& /tmp/log_sum_drone_4_16_3_120_0.txt 2>&1  &
-nohup python3 C_WSP_update_2022_check.py 5 dy2022_16 2 2 0 4 >& /tmp/log_sum_drone_4_16_3_120_0.txt 2>&1  &
-nohup python3 C_WSP_update_2022_check.py 5 dy2022_16 2 2 0 5 >& /tmp/log_sum_drone_4_16_3_120_0.txt 2>&1  &
